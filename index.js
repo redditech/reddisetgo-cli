586af5a5853360e294732543e0d5965ac4ebebf3
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-30 19:10:44 +1000

    adds function to install Near CLI if not present

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
+import { exec } from 'child_process';
+import util from 'util';
 
 (async () => {
     let blockchain;
+    let demo;
     const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
     async function welcome() {
         figlet(`ReddiSetGo CLI V 0.1`, (err, data) => {
@@ -20,7 +23,7 @@ import { createSpinner } from 'nanospinner';
         const answers = await inquirer.prompt({
             name: 'blockchain',
             type: 'list',
-            message: 'Which blockchain do you want to demo? (default is `Near`',
+            message: 'Which blockchain do you want to demo? (default is `Near`)',
             choices: ['Ethereum', 'Solana', 'Near', 'Quit'],
             default() {
                 return 'Near';
@@ -31,22 +34,17 @@ import { createSpinner } from 'nanospinner';
     }
     async function demoBlockchain(selectedBlockchain) {
         const spinner = createSpinner('Prepping demo...').start();
-        await sleep();
         if (selectedBlockchain === 'Quit') {
             spinner.error({ text: `Quitting...` });
             process.exit(1);
         }
         else {
+            await sleep();
             spinner.success({ text: `Nice. I'll prep the demos for ${selectedBlockchain}` });
             switch (selectedBlockchain) {
                 case 'Near':
                     console.clear();
-                    const rainbowTitle = chalkAnimation.rainbow(
-                        "Starting Demos for Near..."
-                    );
-                    await sleep();
-                    rainbowTitle.stop();
-                    await demoNear();
+                    await confirmNearCli() ? await demoNear() : await installNearCli();
                     break;
                 case 'Ethereum':
                     console.log(chalk.red('Still a todo for Ethereum demos'));
@@ -59,8 +57,104 @@ import { createSpinner } from 'nanospinner';
         console.log(chalk.bgGreen(`Returning to menu`));
         await whichBlockchain();
     }
+    async function confirmNearCli() {
+
+        console.log(`I will be executing the following command: ${chalk.blue('near --version')}`);
+        // Reference https://stackabuse.com/executing-shell-commands-with-node-js/
+        // Reference https://zaiste.net/posts/nodejs-child-process-spawn-exec-fork-async-await/
+        const _exec = util.promisify(exec);
+        const rainbowTitle = chalkAnimation.radar(
+            "Checking for the Near CLI..."
+        );
+        try {
+            let { stdout, stderr, error } = await _exec("near --version");
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return false;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return false;
+            }
+            console.log(stdout);
+        }
+        catch (error) {
+            console.log(`error: ${error.message}`);
+            return false;
+        }
+
+        console.log("The Near CLI is already installed");
+        return true;
+    }
+
+    async function installNearCli() {
+        console.log("The Near CLI is not installed. I will install it now");
+        console.log(`I will be executing the following command: ${chalk.blue('npm i -g near-cli')}`);
+        const _exec = util.promisify(exec);
+        try {
+            let { stdout, stderr, error } = await _exec("npm i -g near-cli");
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return false;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                if (stderr.includes("npm ERR!"))
+                    return false; // This is as `npm WARN` also triggers an error but isn't a fail
+                else {
+                    console.log(`stdout: ${stdout}`);
+                    console.log("The Near CLI has been installed, please try re-running the demo");
+
+                    return true;
+                }
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log("The Near CLI has been installed, please try re-running the demo");
+            rainbowTitle.stop();
+        }
+        catch (error) {
+            console.log(`error: ${error.message}`);
+            return false;
+        }
+
+        return true;
+    }
     async function demoNear() {
-        return;
+        const rainbowTitle = chalkAnimation.rainbow(
+            "Starting Demos for Near..."
+        );
+        await sleep();
+        rainbowTitle.stop();
+        const answers = await inquirer.prompt({
+            name: 'near_demo',
+            type: 'list',
+            message: 'Which demo? Default is `Quit`',
+            choices: ['Login', 'Deploy Contract', 'Quit'],
+            default() {
+                return 'Quit';
+            },
+        });
+        demo = answers.near_demo;
+        switch (demo) {
+            case 'Login':
+                console.clear();
+                const rainbowTitle = chalkAnimation.rainbow(
+                    "Starting Login Demo for Near..."
+                );
+                await sleep();
+                rainbowTitle.stop();
+                demoNearLogin();
+                break;
+            default:
+                console.log(chalk.blue('Work in progress'));
+                break;
+        }
+
+    }
+    async function demoNearLogin() {
+        console.log(`This is following docs at 
+                    ${chalk.green('https://docs.near.org/docs/tools/near-cli#near-login'
+        )}`);
     }
     await welcome();
     await whichBlockchain();
